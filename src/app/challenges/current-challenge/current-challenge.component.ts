102aa010c38f79be883dcf01f1ceff4ec4e90142
nipap <stefan.papin@gmail.com>
2019-03-20 06:35:39 +0100

    added auth and today components

---
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from "@angular/core";
+import { ItemEventData } from "tns-core-modules/ui/list-view";
+
+@Component({
+    selector: "ns-current-challenge",
+    templateUrl: "./current-challenge.component.html",
+    styleUrls: ["./current-challenge.component.css"],
+    moduleId: module.id
+})
+export class CurrentChallengeComponent {
+    @Input() challenges: string[] = [];
+
+    onItemTap(args: ItemEventData) {
+        console.dir(args);
+    }
+}
